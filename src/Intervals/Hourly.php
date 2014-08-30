f4d008a2191bdf33190d3b4ae669237dbc9d7b57
Barry Rhodes <barry@insidefoundry.co.uk>
2014-08-30 10:30:49 +0100

    Hourly interval done

--- src/Intervals/Hourly.php
+++ src/Intervals/Hourly.php
@@ -1,32 +1,107 @@
 <?php
 namespace MML\Booking\Intervals;
 
+use MML\Booking\Exceptions;
 use MML\Booking\Interfaces;
 
 class Hourly extends Base implements Interfaces\Interval
 {
+    protected $name     = 'Hourly';
+    protected $plural   = 'Hours';
+    protected $singular = 'Hour';
+
+    protected $hourStarts = '00';
+
+    /**
+     *
+     * @param  DateTime $RoughStart A time near to the desired start
+     * @return DateTime             The exact time on the hour
+     */
     public function getNearestStart(\DateTime $RoughStart)
     {
-        // @todo missing function
+        return $this->nearestTo($RoughStart);
     }
 
+    /**
+     *
+     * @param  DateTime $RoughEnd   A time near to the desired end
+     * @return DateTime             The exact time on the hour
+     */
     public function getNearestEnd(\DateTime $RoughEnd)
     {
-        // @todo missing function
+        return $this->nearestTo($RoughEnd);
+    }
+
+    /**
+     * Because of the nature of an hour, the start and end can be calculated in the same way!
+     *
+     * @param  DateTime $Rough A time near to the desired start point
+     * @return DateTime        The exact time on the hour
+     */
+    protected function nearestTo(\DateTime $Rough)
+    {
+        $Exact = clone $Rough;
+        $Exact->setTime($Exact->format('H'), $this->hourStarts, '00');
+
+        $Diff = $Exact->diff($Rough);
+        $difference = intval($Diff->s) + (intval($Diff->i) * 60);
+
+        if ($difference > 1800) {
+            $Exact->modify('+1 hour');
+        }
+
+        return $Exact;
     }
 
     public function calculateEnd(\DateTime $Start, $qty = 1)
     {
-        // @todo missing function
+        $qty = intval($qty);
+        if ($qty < 1) {
+            throw new Exceptions\Booking("Intervals\Hourly::calculateEnd requires a positive integer $qty");
+        }
+
+        $End = clone $Start;
+        $End->modify("+{$qty} hour");
+        return $End;
     }
 
     public function calculateStart(\DateTime $End, $qty = 1)
     {
-        // @todo missing function
+        $qty = intval($qty);
+        if ($qty < 1) {
+            throw new Exceptions\Booking("Intervals\Hourly::calculateStart requires a positive integer $qty");
+        }
+
+        $Start = clone $End;
+        $Start->modify("-{$qty} hour");
+        return $Start;
     }
 
-    public function configure()
+    public function configure($hourStarts = '00', $name = null, $plural = null, $singular = null)
     {
-        // @todo missing function
+        if (!preg_match('/[0-5][0-9]/', $hourStarts)) {
+            throw new Exceptions\Booking("Intervals\Hourly::configure Invalid start time {$hourStarts}.");
+        }
+        $this->name       = is_null($name)     ? $this->name     : $name;
+        $this->plural     = is_null($plural)   ? $this->plural   : $plural;
+        $this->singular   = is_null($singular) ? $this->singular : $singular;
+
+        $this->hourStarts = $hourStarts;
+    }
+
+    public function setup($reconfigured = false)
+    {
+        $this->Entity->setName($this->name);
+        $this->Entity->setPlural($this->plural);
+        $this->Entity->setSingular($this->singular);
+
+        // if we've just been reconfigured, this needs to be overwritten so don't fetch
+        $starts = ($reconfigured) ? false : $this->Entity->getMeta('hourStarts', false);
+
+        if ($starts && preg_match('/[0-5][0-9]/', $starts)) {
+            $this->hourStarts = $starts;
+        } else {
+            $this->Entity->setMeta('hourStarts', $this->hourStarts);
+        }
     }
 }
---
+++ tests/unit/HourlyTest.php
@@ -0,0 +1,105 @@
+<?php
+namespace MML\Booking\Intervals;
+
+class HourlyTest extends \PHPUnit_Framework_TestCase
+{
+    protected $Object;
+    protected $Persist;
+
+    protected function setUp()
+    {
+        $this->Persist = $this->getMock('MML\\Booking\\Interfaces\\IntervalPersistence');
+        $this->Object = new Hourly($this->Persist);
+    }
+
+    /**
+     * @dataProvider nearesrStartData
+     */
+    public function testGetNearestStart($hourStarts, $in, $out)
+    {
+        $this->Object->configure($hourStarts);
+
+        $Rough = new \DateTime($in);
+        $Exact = $this->Object->getNearestStart($Rough);
+        $this->assertEquals($out, $Exact->format('Y-m-d H:i:s'));
+    }
+
+    /**
+     * @dataProvider nearesrStartData
+     *
+     * For this interval, same input should give the same output
+     */
+    public function testGetNearestEnd($hourStarts, $in, $out)
+    {
+        $this->Object->configure($hourStarts);
+
+        $Rough = new \DateTime($in);
+        $Exact = $this->Object->getNearestStart($Rough);
+        $this->assertEquals($out, $Exact->format('Y-m-d H:i:s'));
+    }
+
+    /**
+     * @dataProvider startEndData
+     */
+    public function testCalculateEnd($hourStarts, $start, $end, $qty)
+    {
+        $this->Object->configure($hourStarts);
+        $Start = new \DateTime($start);
+
+        $Calculated = $this->Object->calculateEnd($Start, $qty);
+        $this->assertEquals($end, $Calculated->format('Y-m-d H:i:s'));
+    }
+    /**
+     * @dataProvider startEndData
+     */
+    public function testCalculateStart($hourStarts, $start, $end, $qty)
+    {
+        $this->Object->configure($hourStarts);
+        $End = new \DateTime($end);
+
+        $Calculated = $this->Object->calculateStart($End, $qty);
+        $this->assertEquals($start, $Calculated->format('Y-m-d H:i:s'));
+    }
+
+    public function nearesrStartData()
+    {
+        return array(
+            array('00', '1982-09-04 23:15:39', '1982-09-04 23:00:00'),
+            array('00', '1982-09-04 23:05:20', '1982-09-04 23:00:00'),
+            array('00', '1982-09-04 23:25:07', '1982-09-04 23:00:00'),
+            array('00', '1982-09-04 23:29:59', '1982-09-04 23:00:00'),
+            array('00', '1982-09-04 23:30:00', '1982-09-04 23:00:00'),
+            array('00', '1982-09-04 22:45:39', '1982-09-04 23:00:00'),
+            array('00', '1982-09-04 22:55:20', '1982-09-04 23:00:00'),
+            array('00', '1982-09-04 22:35:07', '1982-09-04 23:00:00'),
+            array('00', '1982-09-04 22:31:59', '1982-09-04 23:00:00'),
+            array('00', '1982-09-04 22:30:01', '1982-09-04 23:00:00'),
+            array('00', '1982-09-04 23:31:00', '1982-09-05 00:00:00'),
+            array('00', '1982-09-04 23:30:15', '1982-09-05 00:00:00'),
+
+
+            array('15', '1982-09-04 23:15:39', '1982-09-04 23:15:00'),
+            array('15', '1982-09-04 23:05:20', '1982-09-04 23:15:00'),
+            array('15', '1982-09-04 23:25:07', '1982-09-04 23:15:00'),
+            array('15', '1982-09-04 23:29:59', '1982-09-04 23:15:00'),
+            array('15', '1982-09-04 23:30:00', '1982-09-04 23:15:00'),
+            array('15', '1982-09-04 22:45:39', '1982-09-04 23:15:00'),
+            array('15', '1982-09-04 22:55:20', '1982-09-04 23:15:00'),
+            array('15', '1982-09-04 22:35:07', '1982-09-04 22:15:00'),
+            array('15', '1982-09-04 22:31:59', '1982-09-04 22:15:00'),
+            array('15', '1982-09-04 22:30:01', '1982-09-04 22:15:00'),
+            array('15', '1982-09-04 23:31:00', '1982-09-04 23:15:00'),
+            array('15', '1982-09-04 23:30:15', '1982-09-04 23:15:00'),
+        );
+    }
+    public function startEndData()
+    {
+        return array(
+            array('00', '1982-09-04 23:00:00', '1982-09-05 00:00:00', 1),
+            array('00', '1982-09-04 22:00:00', '1982-09-04 23:00:00', 1),
+            array('00', '1982-09-04 21:00:00', '1982-09-04 22:00:00', 1),
+            array('00', '1982-09-04 20:00:00', '1982-09-04 23:00:00', 3),
+            array('00', '1982-09-04 23:00:00', '1982-09-05 02:00:00', 3),
+        );
+    }
+}
