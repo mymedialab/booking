373fec2bcc6dbb88adaed7bec041110b782ea0a9
Barry Rhodes <barry@insidefoundry.co.uk>
2014-09-11 15:36:58 +0100

    Bugfix, periods now correctly overlap as true if they match exactly.

--- src/Availability/IntervalBacked.php
+++ src/Availability/IntervalBacked.php
@@ -31,9 +31,9 @@ class IntervalBacked extends Base implements Interfaces\Availability
         list($Start, $End) = $this->getAvailabilityStartAndEnd($Period);
 
         return (
-            ($Period->getStart() > $Start && $Period->getStart() < $End) ||
-            ($Period->getEnd()   > $Start && $Period->getEnd()   < $End) ||
-            ($Period->getStart() < $Start && $Period->getEnd()   > $End)
+            ($Period->getStart() >= $Start && $Period->getStart() <= $End) ||
+            ($Period->getEnd()   >= $Start && $Period->getEnd()   <= $End) ||
+            ($Period->getStart() <= $Start && $Period->getEnd()   >= $End)
         );
     }
 
