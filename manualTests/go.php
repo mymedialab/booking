5d6ac0bdeba7689d73132175ae84a0e3eef0280e
Barry Rhodes <barry@insidefoundry.co.uk>
2014-10-02 11:45:19 +0100

    Added a method to block reservations to check overlap.

--- manualTests/go.php
+++ manualTests/go.php
@@ -20,13 +20,18 @@ function expectThrow($id, $fn, $message = null)
     die("missing expected exception $id \n\n");
 }
 
-function assertEquals($a, $b)
+function assertEquals($a, $b, $message = null)
 {
     if ($a !== $b) {
         echo "following are unequal: ";
         var_dump($a);
         var_dump($b);
         echo "\n";
+        if ($message) {
+            echo "Message Provided was\n";
+            echo $message;
+            echo "\n";
+        }
         die();
     }
 }
@@ -94,22 +99,48 @@ $Setup    = new Booking\Setup($Factory);
 try {
 
     $Resource = $Setup->createResource('blocktest_something', 'This thing here', 2);
+    $Setup->addBookingIntervals($Resource, array());
 
     $IntervalFactory = $Factory->getIntervalFactory();
 
-    $RecurringInterval = $IntervalFactory->get('Daily');
+    $RecurringInterval = $IntervalFactory->get('Weekly');
     $BookingInterval   = $IntervalFactory->get('TimeOfDay');
 
-    $Resource = $Booking->getResource('blocktest_something');
-    $Start = date_create_from_format('d/m/Y', '04/09/1982');
-        $End   = date_create_from_format('d/m/Y', '10/01/2011');
+    $Start = date_create_from_format('d/m/Y H:i', '04/09/1982 00:00');
+    $End   = date_create_from_format('d/m/Y H:i', '10/01/2011 23:59');
 
-    $RecurringInterval->configure('10:00', '12:30', 'This is the recurring interval');
-    $BookingInterval->configure('10:00', '12:30', 'some friendly name for booking interval');
+    $RecurringInterval->configure($Start, $End, 'Recurring interval'); // end is irrelevant
+    $BookingInterval->configure('00:00', '23:59', 'All day.');
 
-    $Booking->createBlockReservation($Resource, $BookingInterval, $RecurringInterval, $Start, $End); // no end!
+    // Limited runs weekly from my birthday to Finleys birthday. Unlimited runs through until infiinity
+    $Booking->createBlockReservation('Limited Reservation', $Resource, $BookingInterval, $RecurringInterval, $Start, $End);
+    $Booking->createBlockReservation('unlimited Reservation', $Resource, $BookingInterval, $RecurringInterval, $Start); // no end!
 
     $Booking->persist();
+
+    assertEquals(2, count($Resource->getBlockReservations()), "Block reservations found!");
+    // between birthdays, should return 2.
+    assertEquals(2, count($Resource->getBlockReservationsAfter(new \DateTime('2000-07-15 00:00:00'))), "Block reservations after 2000 found");
+    // After Fin's birthday, return one
+    assertEquals(1, count($Resource->getBlockReservationsAfter(new \DateTime('2011-07-15 00:00:00'))), "Block reservations after 2011 found");
+
+    // Any Saturdays prior to my birthday should return 2. Any after finleys birthday should return 1.
+    $Availability = $Factory->getReservationAvailability();
+    $Period = new Booking\Periods\Standalone();
+    $Period->setDuration(new \DateInterval('PT23H59M'));
+
+    // first check that we do have 2 things available on a day other than Saturday...
+    $Period->begins(new \DateTime('2010-07-15 00:00:00'));
+    assertEquals(true, $Availability->check($Resource, $Period, 2), "Two available on a " . $Period->getStart()->format('l'));
+
+    // Now check that we only have 1 thing available on a Saturday after Fins B'day
+    $Period->begins(new \DateTime('2011-07-16 00:00:00'));
+    assertEquals(true, $Availability->check($Resource, $Period, 1), "One available on a " . $Period->getStart()->format('l'));
+    assertEquals(false, $Availability->check($Resource, $Period, 2), "Two UNavailable on a " . $Period->getStart()->format('l'));
+
+    // Now check that we have nothing available on a Saturday before Fins B'day
+    $Period->begins(new \DateTime('2000-07-15 00:00:00'));
+    assertEquals(false, $Availability->check($Resource, $Period, 1), "One UNavailable on a " . $Period->getStart()->format('l'));
 
 } catch (\Exception $e) {
     echo "UNCAUGHT EXCEPTION MUPPET!\n";
--- src/BlockReservations/Base.php
+++ src/BlockReservations/Base.php
@@ -41,7 +41,40 @@ class Base implements Interfaces\BlockReservation
      */
     public function overlaps(Interfaces\Period $Period)
     {
+        $Start  = $this->Entity->getFirstBooking();
+        $Cutoff = $this->Entity->getCutoff(); // may be null!
 
+        if ($Period->getEnd() < $Start) {
+            // booking won't overlap as it's not started yet.
+            return false;
+        }
+        if ($Cutoff && $Period->getStart() > $Cutoff) {
+            // booking won't overlap as our run will finish before the start
+            return false;
+        }
+
+        $Repeat  = $this->getRepeatInterval();
+        $Booking = $this->getBookingInterval();
+
+        // step through planned bookings before period ends or when we hit our [optional] cutoff.
+        while ($Start < $Period->getEnd() && (!$Cutoff || $Start < $Cutoff)) {
+            // find the end of this proposed booking.
+            $End = $Booking->calculateEnd($Start);
+
+            // @todo this overlap logic peppers the codebase. Need to make a utility. Already had a few bugs from it!
+            if (($Period->getStart() >= $Start && $Period->getStart() < $End) ||
+                ($Period->getEnd()   > $Start && $Period->getEnd()   <= $End) ||
+                ($Period->getStart() <= $Start && $Period->getEnd()   >= $End)) {
+                // soon as one overlaps, return
+                return true;
+            }
+
+            // That booking didn't hit, wind on to the next one.
+            $Start = $Repeat->getNextFrom($End);
+        }
+
+        // found
+        return false;
     }
 
     public function getRepeatInterval()
